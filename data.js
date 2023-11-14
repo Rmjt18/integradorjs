61c7575270a259d307c9b102b9aa7896ca55a608
Rodrigo <rjimenez1895@gmail.com>
2023-11-14 00:48:40 -0300

    va

---
+++ data.js
@@ -0,0 +1,99 @@
+const tratamientosdata = [ 
+    {id: 1,
+        name: "Plan Presencial Mensual",
+        category: "presencial",
+        price: 51700,
+        consultas: 4,
+        Prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "/img/presencial a.jpeg",
+    
+        
+    },
+    
+    {
+        id: 2,
+        name: "Plan Presencial Trimestral",
+        category : "presencial",
+        price: 131900,
+        Consultas: 12,
+        Prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/presencial b.jpeg",
+    },
+    
+    {
+        id: 3,
+        name: "Plan Semipresencial Mensual",
+        category: "mixto",
+        price: 44500,
+        Consultas: 4,
+        prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/semi presencial a.jpeg",
+    },
+    
+    {
+        id: 4,
+        name: "Plan Semipresencial Trimestral",
+        category: "mixto",
+        price: 113500,
+        Consultas: 12,
+        prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/semipresencial b.jpeg",
+    },
+    
+    {
+        id: 5,
+        name: "Plan Online Mensual",
+        category: "online",
+        price: 36500,
+        Consultas: 4,
+        Prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/online a.jpeg",
+    },
+    {
+        id: 6,
+        name: "Plan Online Trimestral",
+        category: "online",
+        price: 93100,
+        Consultas: 12,
+        Prestaciones: ["Medico", "Nutricion", "Psicologo", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/online b.jpeg",
+    },
+    {
+        id: 7,
+        name: "Plan Tgi Mensual",
+        category: "online",
+        price: 19400,
+        Consultas: 1,
+        Prestaciones: [ "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/tgi a.jpeg",
+    },
+    {
+        id: 8,
+        name: "Plan Tgi Trimestral",
+        category: "online",
+        price: 49500,
+        Consultas: 3,
+        Prestaciones:[ "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/tgi b.jpeg",
+    },
+    
+    {
+        id: 9,
+        name: "Plan Mantenimiento Mensual",
+        category: "mixto",
+        price: 31800,
+        Consultas: 2,
+        Prestaciones: ["Medico", "Nutricion", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/anana.png",
+    },
+    {
+        id: 10,
+        name: "Plan Mantenimiento Trimestral",
+        category: "mixto",
+        price: 81100,
+        Consultas: 6,
+        Prestaciones: ["Medico", "Nutricion", "Grupos Educativos", "Actividad Fisica "],
+        img: "./img/anana.png",
+    },
+    
+]
