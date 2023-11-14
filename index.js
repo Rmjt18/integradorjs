3ae0d5f4f8f7e774306222e7a14c2fbbc12d3dc6
Rodrigo <rjimenez1895@gmail.com>
2023-11-14 01:25:17 -0300

    save for today

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log(tratamientosdata);
+// contenedor de tratamientos
+
+const tratamientoscontainer= document.querySelector(".tratamientos-container");
+
+// funcion para crear el htl del tratamiento
+
+
+
+const createtratamientotemplate=  (tratamiento) => {
+const {id, name, price, consultas, img} = tratamiento;
+    return`  <div class="pokemon">
+    <img src="${img}" alt="ivysaur">
+    <div class="pokemon__info">
+        <div class="pokemon__info--top">
+            <h3>${name}</h3>
+        </div>
+        <div class="pokemon__info--mid">
+            <div class="pokemon__info--mid--type">
+                <img src="img/tipoplanta.jpeg" alt="tipoplanta">
+                <span>${price}</span>
+            </div>
+            </div>
+            <div class="pokemon__info--down">
+                <p>Fuerte contra - tipo Agua 💪<br>
+                Debil contra - tipo fuego 😵‍💫</p>
+            </div>
+            
+    </div>
+    
+    <div> <a class="btn" href="#"> Evolucionar</a></div>
+    
+</div>`
+    
+
+}
+// funcion para renderizar
+const rendertratamientos = (tratamientoslist) => {
+    tratamientoscontainer.innerHTML += tratamientoslist.map(createtratamientotemplate).join("")
+
+};
+
+// funcion init
+const init = () => {
+    // console.log (tratamientosdata)
+    rendertratamientos(tratamientosdata);
+
+};
+
+init()
