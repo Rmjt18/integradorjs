9d531df28ecfee0cacb71edb88164dc7c8cf67c3
Rodrigo <rjimenez1895@gmail.com>
2023-11-16 23:00:12 -0300

    prue

--- index.js
+++ index.js
@@ -200,8 +200,8 @@ const closeOnScroll= () => {
 
 const addtratamiento= (e) => 
 console.log(e.target);
-if(!e.target.classList.contains(btn-add))return;
-console.log(e.target)
+if(!e.target.classList.contains("btn-add"))return;
+const product = e.target.dataset;
 
 // funcion init
 const init = () => {
