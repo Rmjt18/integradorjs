708fb4311881ddaf767a4ca8281142724cbe9527
Rodrigo <rjimenez1895@gmail.com>
2023-11-19 20:52:18 -0300

    fin

--- register.js
+++ register.js
@@ -1,5 +1,40 @@
-const registerFor = document.querySelector(".container-form");
+const registerForm = document.querySelector(".container-form");
 const nameInput = document.querySelector(".name")
 const emailInput = document.querySelector(".email")
 const phoneInput = document.querySelector (".phone");
 
+// funciones auxiliares ---------------------------
+// VACIO-------------------
+const isEmpty = (input) => {
+    return !input.value.trim().length;
+
+}
+// LARGO----------------
+const  isBetween= (input, min, max ) => {
+return input.value.length >= min && input.value.length <= max;
+}
+
+const showError = (input, message) => {
+    const formField= input.parentElement;
+    formField.classList.add("error")
+    console.log(formField);
+
+}
+
+
+
+
+
+const checkInput = (input) => {
+//cpdifo a checkear 
+// console.log( "empty" + isEmpty(input));
+// console.log( "largo" + isBetween(input, 3 , 25));
+console.log(showError(nameInput))
+}
+
+
+const inicio = () => {
+    nameInput.addEventListener("input", () => checkInput(nameInput));
+};
+
+inicio();
